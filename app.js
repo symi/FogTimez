c18ddf8debef6d9fa0776c970bd4ccff1d122125
Simon Levy <simonjlevy@gmail.com>
2015-11-01 23:09:31 +0000

    added side menu nav and swipe to open/close

--- app.js
+++ app.js
@@ -1,17 +1,61 @@
-function ftToolbar () {
+function ftToolbarDirective () {
     return {
         templateUrl: 'ftToolbar.html',
         restrict: 'E'        
     };
 }
 
-function ftOverflowMenu () {
+function ftOverflowMenuDirective () {
     return {
         templateUrl: 'ftOverflowMenu.html',
         restrict: 'E'        
     };
 }
 
+function ftNavDirective () {
+    return {
+        templateUrl: 'ftNav.html',
+        restrict: 'E'        
+    };
+}
+
+function ftDirective (ftMenuService) {
+    return {
+        templateUrl: 'ft.html',
+        restrict: 'E',
+        scope: true,
+        controller: function ($scope) {
+            $scope.openNav = function () {
+                ftMenuService.requestOpen();
+            };
+            
+            $scope.closeNav = function () {
+                ftMenuService.requestClose();
+            };
+        }        
+    };
+}
+
+function ftMenuService ($mdSidenav) {
+    var componentId = 'menu';
+    
+    function open() {
+        $mdSidenav(componentId).open();
+    }
+    
+    function close() {
+        $mdSidenav(componentId).close();
+    }
+    
+    return {
+        requestOpen: open,
+        requestClose: close,
+    }
+}
+
 angular.module( 'fog-timez', ['ngMaterial'])
-    .directive("ftOverflowMenu", ftOverflowMenu)
-    .directive("ftToolbar", ftToolbar);
+    .factory("ftMenuService", ['$mdSidenav', ftMenuService])
+    .directive("ft", ['ftMenuService', ftDirective])
+    .directive("ftNav", ftNavDirective)
+    .directive("ftOverflowMenu", ftOverflowMenuDirective)
+    .directive("ftToolbar", ftToolbarDirective);
